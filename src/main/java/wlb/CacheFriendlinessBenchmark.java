f15f63ee2b879a1aabb210c4647c4a2886fc6673
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2019-11-06 10:15:10 +0100

    Use Blackhole instead of sink

--- src/main/java/wlb/CacheFriendlinessBenchmark.java
+++ src/main/java/wlb/CacheFriendlinessBenchmark.java
@@ -1,9 +1,15 @@
 package wlb;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+
+import static java.util.Collections.shuffle;
 
 @Fork(3)
 @BenchmarkMode(Mode.AverageTime)
@@ -24,52 +30,46 @@ public class CacheFriendlinessBenchmark {
     public void setup() {
         Random r = new Random(-18379);
         values = new ArrayList<>(size);
-        linkedList = new LinkedList<>();
-        arrayList = new ArrayList<>(size);
         array = new int[size];
         for (int i = 0; i < size; i++) {
             values.add(r.nextInt());
         }
+        arrayList = new ArrayList<>(values);
+        shuffle(arrayList, r);
+        linkedList = new ArrayList<>(values);
+        shuffle(linkedList, r);
         for (int i = 0; i < size; i++) {
-            linkedList.add(values.get(r.nextInt(size)));
-            arrayList.add(values.get(r.nextInt(size)));
             array[i] = values.get(r.nextInt(size));
         }
     }
 
     @Benchmark
-    public void linkedList() {
+    public void linkedList(Blackhole bh) {
         if (iterator == null || !iterator.hasNext()) {
             iterator = linkedList.iterator();
         }
         for (int i = 0; i < 1024 && iterator.hasNext(); i++) {
-            sink(iterator.next());
+            bh.consume(iterator.next().intValue());
         }
     }
 
     @Benchmark
-    public void arrayList() {
+    public void arrayList(Blackhole bh) {
         if (iterator == null || !iterator.hasNext()) {
             iterator = arrayList.iterator();
         }
         for (int i = 0; i < 1024 && iterator.hasNext(); i++) {
-            sink(iterator.next());
+            bh.consume(iterator.next());
         }
     }
 
     @Benchmark
-    public void array() {
+    public void array(Blackhole bh) {
         if (index == array.length) {
             index = 0;
         }
         for (int i = 0; i < 1024 && index < array.length; i++) {
-            sink(array[index++]);
+            bh.consume(array[index++]);
         }
     }
-
-    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
-    private void sink(int value) {
-        // intentionally empty
-    }
-
 }
